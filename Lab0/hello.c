dd4c62ca9f3e6967a4f5d220da476f03b906a487
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2021-03-05 12:09:26 +0530

    First Commit

---
+++ Lab0/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main() {
+	printf("Aniruddha Deb\n2020MT60869\n");
+	return;
+}
---
+++ testing/numtests.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <float.h>
+
+int main() {
+	printf("%f\n", DBL_MIN);
+	double x;
+	scanf("%d", &x);
+	printf("%d\n", x);
+	return 0;
+}
